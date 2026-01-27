826e64810702b1a3ec2297739621873b30b9ce5a
PastaMasta <pasta.masta2902@gmail.com>
2026-01-27 21:22:08 +0000

    misc swarm stuff

--- learning/docker-swarm/setup-debian.sh
+++ learning/docker-swarm/setup-debian.sh
@@ -7,23 +7,23 @@ sudo apt update
 ###############################################################################
 # Docker install
 ###############################################################################
+sudo apt install curl -y
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl restart docker
 
 ###############################################################################
 # Docker start
@@ -33,4 +33,4 @@ mkdir -p /vagrant/web
 echo "Hello from $(uname -n)!" > /vagrant/web/index.html
 
 cd /vagrant
-# sudo docker compose up -d
+sudo docker compose up -d
